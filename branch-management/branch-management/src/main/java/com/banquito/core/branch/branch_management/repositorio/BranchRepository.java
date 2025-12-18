cb13e78b53825463f1b01962b886152088053eb3
Stephani Jamilette Rivera Espinosa <sjrivera2@espe.edu.ec>
2025-12-17 20:54:44 -0500

    Delete branch-management/branch-management directory

--- branch-management/branch-management/src/main/java/com/banquito/core/branch/branch_management/repositorio/BranchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.banquito.core.branch.branch_management.repositorio;
-
-import com.banquito.core.branch.branch_management.modelo.Branch;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BranchRepository extends MongoRepository<Branch, String> {
-}
